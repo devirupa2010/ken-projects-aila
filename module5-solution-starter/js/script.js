cacb126a81d7b41612bb8994d9327f2258e2af26
devirupa2010 <devirupa2010@yahoo.com>
2025-12-02 23:55:12 -0500

    Update script.js

--- module5-solution-starter/js/script.js
+++ module5-solution-starter/js/script.js
@@ -18,8 +18,8 @@ var allCategoriesUrl =
   "snippets/category-snippet.html";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
-var menuItemsUrl = 
-  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+var menuItemsUrl =   "snippets/menu-items-snippet.html?category=";
+
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
 var menuItemHtml = "snippets/menu-item.html";
 
